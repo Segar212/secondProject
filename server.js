2c1a63230659adbc8a4b9ba11ad895bf55d6f82e
Ben Angek <ben@Bens-MacBook-Air.local>
2018-02-06 10:57:43 -0700

    file here

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require ("path");
 const app = express();
 
 app.use(‘/‘, express.static(path.join(__dirname, "public")));
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () =>
-console.log ("it works 3000"));
+app.listen(port, () => console.log ("it works 3000"));
